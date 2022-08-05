5c19fbb05eada67cb8951e7c69669e660ccb4449
HAJIWEE <HAJIWEE94.11@gmail.com>
2022-08-05 23:43:45 +0800

    profile and navbar done

--- src/Resources/NavBar.js
+++ src/Resources/NavBar.js
@@ -1,11 +1,44 @@
 import React from "react";
+import { Home, Search, Plus, Buy, User } from "react-iconly";
 import { Outlet, Link } from "react-router-dom";
+import "./NavBar.css";
 
 const Nav = () => {
   return (
-    <div>
-      <h1>This is for the Nav Bar</h1>
-    </div>
+    <>
+      <Outlet />
+      <div className="navbar">
+        <ul className="navbar li">
+          <li>
+            <Link to="/newsfeed">
+              <Home set="bold" primaryColor="black" />
+            </Link>
+          </li>
+          <li>
+            <Link to="/search">
+              <Search set="Light-Outline" primaryColor="#878D98" />
+            </Link>
+          </li>
+          <li>
+            <Link to="/upload">
+              <Plus set="Light-Outline" primaryColor="#878D98" />
+            </Link>
+          </li>
+          <li>
+            <Link to="/cart">
+              <Buy set="Light-Outline" primaryColor="#878D98" />
+            </Link>
+          </li>
+          <li>
+            <Link to="/profile">
+              <div className="profile">
+                <User set="bold" primaryColor="#878D98" />
+              </div>
+            </Link>
+          </li>
+        </ul>
+      </div>
+    </>
   );
 };
 
---
+++ src/Resources/uploadpicture.js
@@ -0,0 +1,8 @@
+import React from "react";
+import { Outlet, Link } from "react-router-dom";
+
+const UploadPicture = () => {
+  return <div>this is the uploadpicture page</div>;
+};
+
+export { UploadPicture };
