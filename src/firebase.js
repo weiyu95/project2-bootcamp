2e2c657c9b48b2d5ad5b15e649aa2983527eed53
HAJIWEE <99461740+HAJIWEE@users.noreply.github.com>
2022-08-09 23:53:38 +0800

    Update firebase.js

--- src/firebase.js
+++ src/firebase.js
@@ -6,22 +6,13 @@ import { getStorage } from "firebase/storage";
 
 // TODO: Replace with your app's Firebase project configuration
 const firebaseConfig = {
-  apiKey: "AIzaSyCczisxSfdXKEVZ_qN5l-wFTL5LNpkR-vU",
-  authDomain: "fir-e044c.firebaseapp.com",
-  // The value of `databaseURL` depends on the location of the database
-  databaseURL:
-    "https://fir-e044c-default-rtdb.asia-southeast1.firebasedatabase.app",
-  projectId: "fir-e044c",
-  storageBucket: "fir-e044c.appspot.com",
-  messagingSenderId: "SENDER_ID",
-  appId: "APP_ID",
-  // apiKey: process.env.REACT_APP_FIREBASE_API_KEY,
-  // authDomain: process.env.REACT_APP_FIREBASE_AUTH_DOMAIN,
-  // databaseURL: process.env.REACT_APP_FIREBASE_DATABASE_URL,
-  // projectId: process.env.REACT_APP_FIREBASE_PROJECT_ID,
-  // storageBucket: process.env.REACT_APP_FIREBASE_STORAGE_BUCKET,
-  // messagingSenderId: process.env.REACT_APP_FIREBASE_MESSAGING_SENDER_ID,
-  // appId: process.env.REACT_APP_APP_ID,
+   apiKey: process.env.REACT_APP_FIREBASE_API_KEY,
+   authDomain: process.env.REACT_APP_FIREBASE_AUTH_DOMAIN,
+   databaseURL: process.env.REACT_APP_FIREBASE_DATABASE_URL,
+   projectId: process.env.REACT_APP_FIREBASE_PROJECT_ID,
+   storageBucket: process.env.REACT_APP_FIREBASE_STORAGE_BUCKET,
+   messagingSenderId: process.env.REACT_APP_FIREBASE_MESSAGING_SENDER_ID,
+   appId: process.env.REACT_APP_APP_ID,
 };
 
 // Initialize Firebase
