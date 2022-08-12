fb3bbc8026ead45617a97ac2b17d21409bcd76a3
weiyu95 <weiyu95@gmail.com>
2022-08-12 23:07:14 +0800

    able to display cart item from database and update the database when user click Order or Remove button but display updates only upon refresh

--- src/Resources/Cart.js
+++ src/Resources/Cart.js
@@ -1,40 +1,79 @@
 import React, { useState, useEffect } from "react";
-import { onChildAdded, ref as databaseRef } from "firebase/database";
+import {
+  push,
+  onChildAdded, onValue,
+  ref as databaseRef,
+  update,
+  set,
+} from "firebase/database";
 import { database } from "../firebase";
 import Card from "react-bootstrap/Card";
 import Button from "react-bootstrap/Button";
 import { Outlet, Link } from "react-router-dom";
 
 const USERS_FOLDER_NAME = "users";
+const USER_ORDERS_NAME = `${USERS_FOLDER_NAME}/user/orders`;
 
 const Cart = ({ user }) => {
   const [userCartItems, setUserCartItems] = useState([]);
 
   useEffect(() => {
-    const userRef = databaseRef(database, `${USERS_FOLDER_NAME}/${user}/cart`);
+    const userRef = databaseRef(database, `${USERS_FOLDER_NAME}/user/cart`);
     onChildAdded(userRef, (data) => {
-      setUserCartItems([...data, { key: data.key, val: data.val() }]);
+      setUserCartItems((prevState) => [
+        ...prevState,
+        { key: data.key, val: data.val() },
+      ]);
     });
-  });
+  }, []);
+
+  console.log(userCartItems);
+
+  const handleOrder = (itemOrdered) => {
+    // const userRef = databaseRef(database, `${USERS_FOLDER_NAME}/user/cart`);
+    // onValue(userRef, (snapshot) => {
+    //   const data = snapshot.val();
+    //   console.log(data)
+    //   console.log(data.item3)
+    // })
+    const ordersListRef = databaseRef(database, USER_ORDERS_NAME);
+    const newOrderRef = push(ordersListRef);
+    const index = userCartItems.findIndex((item)=>item.key === itemOrdered)
+    console.log(index)
+    console.log(itemOrdered)
+    set(newOrderRef, userCartItems[index]);
+    handleDelete(itemOrdered);
+  };
+
+  const handleDelete = (itemDeleted) => {
+    console.log(itemDeleted);
+    const updates = {};
+    updates[`/${USERS_FOLDER_NAME}/user/cart/${itemDeleted}`] = null;
+    return update(databaseRef(database), updates);
+  };
 
   let cartCards = userCartItems.map((item) => (
-    <Card style={{ width: "18rem" }} key={item.key}>
+    <Card style={{ width: "18rem", position: "center" }} key={item.key}>
       <Card.Img variant="top" src={item.val.imageLink} />
       <Card.Body>
         <Card.Title>{item.val.itemName}</Card.Title>
         <Card.Subtitle className="mb-2 text-muted">
-          {item.val.itemPrice}
+          ${item.val.itemPrice}
         </Card.Subtitle>
         <Card.Text style={{ fontSize: 20 }}>
           {item.val.itemDescription}
         </Card.Text>
-        <Button variant="primary">Place Order</Button>
-        <Button variant="primary">Remove from Cart</Button>
+        <Button variant="primary" onClick={() => handleOrder(item.key)}>
+          Order
+        </Button>
+        <Button variant="primary" onClick={() => handleDelete(item.key)}>
+          Remove
+        </Button>
       </Card.Body>
     </Card>
   ));
 
-  return cartCards.reverse();
+  return cartCards;
 };
 
 export { Cart };
