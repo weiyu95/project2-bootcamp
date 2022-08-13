ded0a904f2b785c0d4c1c316b6ff29713e5c20ae
HAJIWEE <HAJIWEE94.11@gmail.com>
2022-08-13 12:11:31 +0800

    try upload pic

--- src/firebase.js
+++ src/firebase.js
@@ -6,15 +6,6 @@ import { getStorage } from "firebase/storage";
 
 // TODO: Replace with your app's Firebase project configuration
 const firebaseConfig = {
-<<<<<<< HEAD
-   apiKey: process.env.REACT_APP_FIREBASE_API_KEY,
-   authDomain: process.env.REACT_APP_FIREBASE_AUTH_DOMAIN,
-   databaseURL: process.env.REACT_APP_FIREBASE_DATABASE_URL,
-   projectId: process.env.REACT_APP_FIREBASE_PROJECT_ID,
-   storageBucket: process.env.REACT_APP_FIREBASE_STORAGE_BUCKET,
-   messagingSenderId: process.env.REACT_APP_FIREBASE_MESSAGING_SENDER_ID,
-   appId: process.env.REACT_APP_APP_ID,
-=======
   apiKey: process.env.REACT_APP_FIREBASE_API_KEY,
   authDomain: process.env.REACT_APP_FIREBASE_AUTH_DOMAIN,
   databaseURL: process.env.REACT_APP_FIREBASE_DATABASE_URL,
@@ -22,7 +13,6 @@ const firebaseConfig = {
   storageBucket: process.env.REACT_APP_FIREBASE_STORAGE_BUCKET,
   messagingSenderId: process.env.REACT_APP_FIREBASE_MESSAGING_SENDER_ID,
   appId: process.env.REACT_APP_APP_ID,
->>>>>>> 1966c2b (try upload pic)
 };
 
 // Initialize Firebase
