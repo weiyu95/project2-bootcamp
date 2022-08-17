eebc8f2acfb79bf22edd7ed0583f405c5ec62bdc
weiyu95 <weiyu95@gmail.com>
2022-08-17 17:24:56 +0800

    working liked products page and copied css from Cart.css

--- src/Resources/LikedProducts.js
+++ src/Resources/LikedProducts.js
@@ -8,80 +8,112 @@ import {
   set,
 } from "firebase/database";
 import { database } from "../firebase";
+import { Outlet, Link } from "react-router-dom";
+import "./cssfiles/Liked.css";
+//***imports from images folder***
+import divider from "./images/NavBar Divider.svg";
+import deletesvg from "./images/Delete.svg";
+import cartsvg from "./images/Cart.svg";
+//***imports from react-bootstrap***
 import Card from "react-bootstrap/Card";
 import Button from "react-bootstrap/Button";
-import divider from "./images/NavBar Divider.svg";
-import { Outlet, Link } from "react-router-dom";
+import Container from "react-bootstrap/Container";
+import Row from "react-bootstrap/Row";
+//***imports from react-iconly***
+import { CaretLeft } from "react-iconly";
 
 const USERS_FOLDER_NAME = "users";
 const USER_CART_NAME = `${USERS_FOLDER_NAME}/user/cart`;
 
-const LikedProducts = ({user}) => {
-   const [userLikedItems, setUserLikedItems] = useState([]);
+const LikedProducts = ({ user }) => {
+  const [userLikedItems, setUserLikedItems] = useState([]);
 
-   useEffect(() => {
-     const userRef = databaseRef(database, `${USERS_FOLDER_NAME}/user/liked`);
-     onChildAdded(userRef, (data) => {
-       setUserLikedItems((prevState) => [
-         ...prevState,
-         { key: data.key, val: data.val() },
-       ]);
-     });
-     onChildRemoved(userRef, (data) => {
-       console.log(`${data.val().itemName} removed.`);
-       //onChildRemoved & onChildChange could not get userCartItems.
-       console.log(`current cart: ${userLikedItems}`);
-     });
-   }, []);
+  useEffect(() => {
+    const userRef = databaseRef(database, `${USERS_FOLDER_NAME}/user/liked`);
+    onChildAdded(userRef, (data) => {
+      setUserLikedItems((prevState) => [
+        ...prevState,
+        { key: data.key, val: data.val() },
+      ]);
+    });
+  }, []);
 
-   const handleAddToCart = (itemAddedToCart) => {
-     const ordersListRef = databaseRef(database, USER_CART_NAME);
-     const newOrderRef = push(ordersListRef);
-     const index = userLikedItems.findIndex((item) => item.key === itemAddedToCart);
-     console.log(index);
-     console.log(itemAddedToCart);
-     set(newOrderRef, userLikedItems[index]);
-     handleDelete(itemAddedToCart);
-   };
+  useEffect(() => {
+    const userRef = databaseRef(database, `${USERS_FOLDER_NAME}/user/liked`);
+    onChildRemoved(userRef, (data) => {
+      setUserLikedItems((prev) => {
+        return prev.filter((likedItems) => likedItems.key !== data.key);
+      });
+    });
+  }, []);
 
-   const handleDelete = (itemDeleted) => {
-     const newArray = userLikedItems.filter(
-       (likedItems) => likedItems.key !== itemDeleted
-     );
-     setUserLikedItems(newArray);
-     const updates = {};
-     updates[`/${USERS_FOLDER_NAME}/user/liked/${itemDeleted}`] = null;
-     update(databaseRef(database), updates);
-   };
+  const handleAddToCart = (itemAddedToCart, event) => {
+    event.preventDefault();
+    const ordersListRef = databaseRef(database, USER_CART_NAME);
+    const newOrderRef = push(ordersListRef);
+    const index = userLikedItems.findIndex(
+      (item) => item.key === itemAddedToCart
+    );
+    console.log(index);
+    console.log(itemAddedToCart);
+    set(newOrderRef, userLikedItems[index]);
+    handleDelete(itemAddedToCart);
+  };
 
-   let likedCards = userLikedItems.map((item) => (
-     <Card border="danger" style={{ width: "18rem" }} key={item.key}>
-       <Card.Img variant="top" src={item.val.imageLink} />
-       <Card.Body>
-         <Card.Title>{item.val.itemName}</Card.Title>
-         <Card.Subtitle className="mb-2 text-muted">
-           ${item.val.itemPrice}
-         </Card.Subtitle>
-         <Card.Text style={{ fontSize: 12 }}>
-           {item.val.itemDescription}
-         </Card.Text>
-         <Button variant="primary" onClick={() => handleAddToCart(item.key)}>
-           Add to Cart
-         </Button>
-         <Button variant="primary" onClick={() => handleDelete(item.key)}>
-           Remove
-         </Button>
-       </Card.Body>
-     </Card>
-   ));
+  const handleDelete = (itemDeleted, event) => {
+    event.preventDefault();
+    const newArray = userLikedItems.filter(
+      (likedItems) => likedItems.key !== itemDeleted
+    );
+    setUserLikedItems(newArray);
+    const updates = {};
+    updates[`/${USERS_FOLDER_NAME}/user/liked/${itemDeleted}`] = null;
+    update(databaseRef(database), updates);
+  };
 
-   return (
-     <div>
-       <label className="title">Liked</label>
-       <img className="divider" src={divider} alt="divider" />
-       {likedCards}
-     </div>
-   );
+  let likedCards = userLikedItems.map((item) => (
+    <Card className="likedBox" key={item.key}>
+      <Card.Img variant="top" src={item.val.imageLink} />
+      <Card.Body>
+        <Card.Title>{item.val.itemName}</Card.Title>
+        <Card.Subtitle>
+          ${item.val.itemPrice}
+        </Card.Subtitle>
+        <Card.Text style={{ fontSize: 15 }}>
+          {item.val.itemDescription}
+        </Card.Text>
+        <Button
+          className="likedButtonBox"
+          variant="primary"
+          onClick={(event) => handleAddToCart(item.key, event)}
+        >
+          <img src={cartsvg} alt="Cart svg" /> Add to Cart
+        </Button>
+        <Button
+          className="likedButtonBox"
+          variant="primary"
+          onClick={(event) => handleDelete(item.key, event)}
+        >
+          <img src={deletesvg} alt="Delete svg" /> Delete
+        </Button>
+      </Card.Body>
+    </Card>
+  ));
+
+  return (
+    <Container className="likedPage">
+      <Row className="likedTitleBar">
+        <Link to="/newsfeed">
+          <CaretLeft set="bold" primaryColor="#2FF522" />
+        </Link>
+        <label className="likedTitle">Liked Products</label>
+      </Row>
+      <Row className="likedDivider">
+        <img src={divider} alt="divider" />
+      </Row>
+      <div className="likedCenterViewBox">{likedCards}</div>
+    </Container>
+  );
 };
 
 export { LikedProducts };
